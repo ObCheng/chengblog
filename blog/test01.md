9a49108cb9fea48169299b56f3fcf40fdead85df
Chengobviously <775601647@qq.com>
2023-11-29 12:29:40 +0800

    myblog

---
+++ blog/test01.md
@@ -0,0 +1,19 @@
+---
+slug: test01
+title: test01
+author: 认真学习的小诚
+author_title: 研究僧
+author_url: https://github.com/ObCheng
+author_image_url: ./chenglogo.png
+description: 请输入描述
+tags: [嵌入式, AI, React]
+# activityId: 相关动态 ID
+# bvid: 相关视频 ID（与 activityId 2选一）
+# oid: oid
+---
+
+<!-- truncate -->
+
+
+11111111
+
---
+++ sidebars.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  mySidebar: [{
+    type: 'doc',
+    id: 'all-intro', // 文档 ID
+    label: '开始', // 侧边栏标签
+  },
+  {
+    type: 'category',
+    label: 'html(0)',
+    items: [
+      // "html/01第一个html",
+      // "html/02LocalStorage与SessionStorage",
+      // "html/03HTML DOM节点操作",
+
+    ]
+  },
+  {
+    type: 'category',
+    label: 'Linux驱动入门(1)',
+    items: [
+      "Linux/embedded-linux-start",
+      "Linux/01开发环境搭建"
+
+    ]
+  },
+  {
+    type: 'category',
+    label: 'javascript(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: '微信小程序(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: 'HTTP(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: 'Browser(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: 'Webpack(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: 'git代码托管(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: '数据结构与算法(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: '设计模式(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: '编码规范(0)',
+    items: []
+  },
+  {
+    type: 'category',
+    label: '杂谈(0)',
+    items: []
+  },
+  ],
+};
